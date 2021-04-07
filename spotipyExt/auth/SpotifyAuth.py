5f64af67e97e41130c8d3bf06a6cfaf6031cf954
claytoncuteri <cuteri.clayton28@gmail.com>
2021-04-07 15:22:11 -0700

    clayton's uid.. need to change if using this branch

--- spotipyExt/auth/SpotifyAuth.py
+++ spotipyExt/auth/SpotifyAuth.py
@@ -2,13 +2,13 @@ import json
 import os
 from .. import spotipyExt
 
-CLIENT_SECRETS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),'client_secret_spotify.json')
+CLIENT_SECRETS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),'client_secret.json')
 DEFAULT_SCOPE = 'user-library-read playlist-read-private'
-
+uid = '1251275416'
 def get_uid(client_secret_file = CLIENT_SECRETS_FILE):
-    with open(client_secret_file,'r') as fid:
-        credz = json.load(fid)
-    return credz['userconfig']['uid']
+    #with open(client_secret_file,'r') as fid:
+        #credz = json.load(fid)
+    return uid #credz['userconfig']['uid']
 
 def get_authenticated_service(client_secret_file=CLIENT_SECRETS_FILE, scope=DEFAULT_SCOPE):
     return spotipyExt.initializeSpotifyToken(scope,get_uid(client_secret_file))
